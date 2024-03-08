b41ef20e1b1ae5232edf1d7f79adb48b743ab4c9
Abdi kumela <abdikumela@036gmail.com>
2024-03-08 18:05:23 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,58 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello react!</h1>
+      <Header />
+      <Musics />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header_container">
+      <div>
+        <h1>Music List</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
+          pariatur magni sunt eveniet alias debitis ipsum vitae velit molestias
+          quibusdam.
+        </p>
+      </div>
+    </header>
+  );
+}
+
+function Musics() {
+  return (
+    <main className="main_container">
+      <button className=" Add_song btn">Add Song</button>
+      <MusicInfo />
+      <MusicInfo />
+      <MusicInfo />
+      <MusicInfo />
+    </main>
+  );
+}
+function MusicInfo() {
+  return (
+    <div className="image_container">
+      <a href>
+        <img src="imgs/Wall-e.jpg" alt="Wall-E" />
+      </a>
+      <h4 className="Music_title">musicTitle:shape of you</h4>
+      <div className="btn_container">
+        <button className="btn">Delete</button>
+        <button className="btn">Update</button>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
+      <footer></footer>
     </div>
   );
 }
