73c5430099c596faa8ac2230ebc3e4872d441906
Abdi Kumela <abdikumela036@gmail.com>
2024-03-09 23:43:06 +0300

    UI update and form render handled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import musicData from "./data.json";
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Header />
-      <List />
+      <MusicForm />
       <Musics />
       <Footer />
     </div>
@@ -14,58 +15,138 @@ function App() {
 
 function Header() {
   return (
-    <header className="header_container">
-      <div>
-        <h1>
-          Music is the universal language that transcends boundaries, speaks to
-          the soul, and paints emotions on the canvas of our existence.
-        </h1>
+    <>
+      <header className="header_container">
+        <div>
+          <h1>
+            Music is the universal language that transcends boundaries, speaks
+            to the soul, and paints emotions on the canvas of our existence.
+          </h1>
+        </div>
+      </header>
+      <div className="music_list">
+        <h2>Music list</h2>
       </div>
-    </header>
-  );
-}
-
-function List() {
-  return (
-    <div className="music_list">
-      <h2>Music list</h2>
-    </div>
+    </>
   );
 }
 
 function Musics() {
   return (
     <main className="main_container">
-      <button className=" Add_song btn">Add Music</button>
       <MusicInfo />
     </main>
   );
 }
 
-function MusicInfo() {
-  return musicData.map((data) => (
-    <div key={data.id} className="image_container">
-      <a href="#">
-        <img src="imgs/Wall-e.jpg" alt="Wall-E" />
-      </a>
-      <h4 className="Music_title">musicTitle:{data.musicTitle}</h4>
-      <h4 className="Music_Artist">Artist:{data.artist}</h4>
-      <h4 className="Music_Duration">Duration:{data.duration}</h4>
-      <h4 className="Music_genre">gener:{data.genre}</h4>
-      <div className="btn_container">
-        <button className="btn">Delete</button>
-        <button className="btn">Update</button>
-      </div>
-    </div>
-  ));
-}
+/////////// a component for adding(rendering) a music from the form //////////
 
-function Footer() {
+function MusicForm() {
+  const [title, setTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [duration, setDuration] = useState("");
+  const [genre, setGenre] = useState("");
+
+    // inner function to handle the submit button
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!title || !artist || !duration || !genre) return;
+    const elements = { title, artist, duration, genre };
+    console.log(elements);
+
+    setTitle("");
+    setArtist("");
+    setDuration("");
+    setGenre("");
+  }
   return (
-    <div className="footer">
-      <footer>@music All rights reserved &copy;</footer>
+    <div className="music_Form_Container">
+      <form onSubmit={handleSubmit}>
+        <label>Music Title:</label>
+        <div>
+          <input
+            type="text"
+            placeholder="insert music title"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+        <label>Artist:</label>
+        <div>
+          <input
+            type="text"
+            placeholder="insert artist name..."
+            value={artist}
+            onChange={(e) => {
+              setArtist(e.target.value);
+            }}
+          />
+        </div>
+        <label>Duration:</label>
+        <div>
+          <input
+            type="text"
+            placeholder="insert the duration"
+            value={duration}
+            onChange={(e) => {
+              setDuration(e.target.value);
+            }}
+          />
+        </div>
+        <label>Genre:</label>
+        <div>
+          <input
+            type="text"
+            placeholder="insert music genre.."
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn">
+          Add Music
+        </button>
+      </form>
+      <div>
+        <p>
+          Music is the universal language that transcends boundaries, speaks to
+          the soul, and paints emotions on the canvas of our existence. Music is
+          the universal language that transcends boundaries, speaks to the soul,
+          and paints emotions on the canvas of our existence. Music is the
+        </p>
+      </div>
     </div>
   );
 }
 
-export default App;
+// ////// a component for musicInformation/////////////
+
+function MusicInfo() {
+  return musicData.map((data) => (
+    <div key={data.id} className="image_container">
+      <a href="#">
+        <img
+          width={"300px"}
+          height={"200px"}
+          src={`imgs/${data.image}`}
+          alt={data.musicTitle}
+        />
+      </a>
+      <h4 className="Music">musicTitle:{data.musicTitle}</h4>
+      <h4 className="Music">Artist:{data.artist}</h4>
+      <h4 className="Music">Duration:{data.duration}</h4>
+      <h4 className="Music">gener:{data.genre}</h4>
+      {/* <div className="btn_container">
+        <button className="btn">Delete</button>
+        <button className="btn">Update</button>
+      </div> */}
+    </div>
+  ));
+}
+
+// /////////a component for Footer///////////
+
+function Footer() {
+  return <div className="footer">@music_page All rights reserved &copy;</div>;
+}
